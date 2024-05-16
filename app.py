f067965ed9624bbd80aed3fa6a267a2607090fd9
Kanha-Codes-XD <85252018+Kanha02052002@users.noreply.github.com>
2024-05-17 03:35:01 +0530

    uploaded all files

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import string
+import pickle
+import webbrowser
+
+global Lrdetect_Model
+
+LrdetectFile=open('model.pckl','rb')
+Lrdetect_Model=pickle.load(LrdetectFile)
+LrdetectFile.close()
+
+st.title("Language Detector Model")
+inputText=st.text_input("Provide your text here")
+buttonCheck=st.button("Check the Language name")
+if buttonCheck:
+    st.text(Lrdetect_Model.predict([inputText]))
